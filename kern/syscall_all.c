17118d2698d24ac3adbde98eb18728e10558aea4
刘志千 <22373024@buaa.edu.cn>
2024-07-19 10:35:17 +0800

    lab5

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -456,7 +456,16 @@ int sys_cgetc(void) {
  */
 int sys_write_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (1/2) */
-
+	if (len != 1 && len != 2 && len != 4) {
+		return -E_INVAL;
+	}
+	if (is_illegal_va_range(va, len)) {
+		return -E_INVAL;
+	}
+	if ((pa < 0x180003f8 || pa + len > 0x180003f8 + 0x20) && (pa < 0x180001f0 || pa + len > 0x180001f0 + 0x8)) {
+		return -E_INVAL;
+	}
+	memcpy((void *)(pa | KSEG1), (void *)va, len);	
 	return 0;
 }
 
@@ -477,7 +486,16 @@ int sys_write_dev(u_int va, u_int pa, u_int len) {
  */
 int sys_read_dev(u_int va, u_int pa, u_int len) {
 	/* Exercise 5.1: Your code here. (2/2) */
-
+	if (len != 1 && len != 2 && len != 4) {
+		return -E_INVAL;
+	}
+	if (is_illegal_va_range(va, len)) {
+		return -E_INVAL;
+        }
+	if ((pa < 0x180003f8 || pa + len > 0x180003f8 + 0x20) && (pa < 0x180001f0 || pa + len > 0x180001f0 + 0x8)) {
+		return -E_INVAL;
+	}
+	memcpy((void *)va, (void *)(pa | KSEG1), len);
 	return 0;
 }
 
